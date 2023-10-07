6cee1b5055780a3ee1524bf45f87cf1f391b351f
Gravity <tevoned@gmail.com>
2023-10-07 15:36:14 -0400

    new project addition

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, request, redirect
-import csv 
+import csv
 
 app = Flask(__name__)
 print(__name__)
@@ -14,38 +14,38 @@ def my_home():
 def html_page(page_name):
     return render_template('page_name')
 
+
 def write_to_file(data):
     with open('database.txt', mode='a') as database:
         email = data["email"]
         subject = data["subject"]
         message = data["message"]
         file = database.write(f'\n{email},{subject},{message}')
-        
+
+
 def write_to_csv(data):
     with open('database.csv', mode='a') as database2:
         email = data["email"]
         subject = data["subject"]
         message = data["message"]
-        csv_writer = csv.writer(database2, delimeter=',', quotechar='"', quoting=csv.QUOTE_MINMAL)
-        csv_writer.writerow([email,subject,message])
+        csv_writer = csv.writer(database2, delimeter=',',
+                                quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
         try:
-            p
             data = request.form.to_dict()
             write_to_file(data)
             return redirect('/thankyou.html')
         except:
             return 'did not save to database'
-    else: 
+    else:
         return 'Something went wrong. Please try again!'
-        
 
 
 # @app.route("/favicon.ico")
 # def blog():
 #     return "<p>Welcome to the number 1 Blog</p>"
-
-
